5e7dcc7b74d496b2360f891278b898ef0c461e3c
martynov <martynov-pashok@rambler.ru>
2023-11-10 17:34:32 +0300

    rebuild for only second values

--- fib/main.cpp
+++ fib/main.cpp
@@ -7,7 +7,9 @@ cin >> N;
 int a = 0, b = 1, nextTerm;
 cout << "Ряд чисел Фибоначчи до " << N << " итераций: " << std::endl;
 for (int i = 1; i < N; ++i) {
+if(i % 2 == 1){
 cout << a << std::endl;
+}
 nextTerm = a + b;
 a = b;
 b = nextTerm;
