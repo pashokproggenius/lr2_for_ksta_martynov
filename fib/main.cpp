4b49e7e1e690cf3c86f19194d8a4415d30ba2633
martynov <martynov-pashok@rambler.ru>
2023-11-10 17:56:48 +0300

    new func

--- fib/main.cpp
+++ fib/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
+
+int fibbonachi(int N){
+    int ticker = 0;
+    int a = 0, b = 1, nextTerm;
+    cout << "Ryad numbers of Fibbonachi do " << N << " iterations: "<< std::endl;
+
+    for(int i = 1; i <= N; i++){
+        if(ticker == 3){
+            cout << a << std::endl;
+            ticker = 0;
+        }
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
+        ticker++;
+    }
+    return 0;
+}
+
 int main() {
-int N;
-cout << "Введите количество итераций N: ";
-cin >> N;
-int a = 0, b = 1, nextTerm;
-cout << "Ряд чисел Фибоначчи до " << N << " итераций: " << std::endl;
-for (int i = 1; i < N; ++i) {
-if(i % 3 == 0){
-cout << a << std::endl;
-}
-nextTerm = a + b;
-a = b;
-b = nextTerm;
-}
-return 0;
+    int iteration = 0;
+    cout << "Input number iterations N: ";
+    cin >> iteration;
+    fibbonachi(iteration);
 }
